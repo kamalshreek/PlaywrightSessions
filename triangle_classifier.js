fc0737eae3949796d4ec530be9774f019a2766dd
kamal <kamalshreek@gmail.com>
2026-02-28 09:29:15 +0530

    chore: commit workspace updates

--- triangle_classifier.js
+++
@@ -1,7 +0,0 @@
-let a = 7,b=9,c=8;
-if ((a == b) && (b == c))
-    console.log("Equilateral Triangle");
-else if (((a == b) &&(a!=c)) || ((b==c)&& (b!=a))|| ((c==a)&&(c!=b)))
-    console.log("Isosceles Triangle");
-else
-    console.log("Scalene Triangle");
